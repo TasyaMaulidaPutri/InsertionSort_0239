d09c9d61c959e709dd17938df5203f1b4a64639f
TasyaMaulidaPutri <tasyamaulidaputri06@gamil.com>
2025-03-25 11:59:36 +0700

    input data

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -1,2 +1,33 @@
 #include <iostream>
 using namespace std;
+
+int arr[20]; // Membuat Array dengan panjang data 20
+int n; // Membuat Variable inputan n
+
+void input()
+{ // Procedure Input
+    while (true)
+{
+    cout << "Masukkan Jumlah Data pada Array : "; // Mmebuat Inputan jumlah element Array
+    cin >> n; // Memanggil variable inputan n
+
+if (n <= 20)
+{ // Membuat Kondisi n tidak lebih dari 20
+break;
+}
+else
+{
+    cout << "\nArray yang anda masukkan maksimal 20 Elemen.\n"; // Menampilkan pesan jika data lebih dari 20
+}
+}
+    cout << endl; // Membuat jarak per baris program
+    cout << "=============" << endl; // Membuat tampilan susunan data element array
+    cout << "Masukkan Element Array" << endl;
+    cout << "=============" << endl;
+
+for (int i = 0; i < n; i++) // Menggunakan perulangan for untuk menyimpan data pada array
+{
+    cout << "Data ke-" << (i + 1) << ":"; // Memasukkan atau menginputkan nilai data n
+    cin >> arr[i]; // Menyimpan nilai data n kedalam array arr
+}
+}
